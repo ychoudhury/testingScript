d8b18dac689ec2da419d4c6760b2be4207d0e23a
Yasir Choudhury <yasirgchoudhury@utexas.edu>
2019-06-20 12:13:33 -0500

    Add files via upload

--- new1.py
+++ new1.py
@@ -3,12 +3,17 @@
 
 import os
 import openpyxl
+from openpyxl.chart import LineChart, Reference
+from openpyxl import load_workbook
 
-os.chdir('C:\\Users\yasirc\Desktop\parseData')
+
+os.chdir('C:\\Users\yasirc\Desktop\parseData') # enter correct filepath for project here
 wb = openpyxl.load_workbook('ngt_log.xlsx')
-sheet = wb['sheet1']
+sheet = wb['sheet1'] # name of the sheet that is being analyzed
 cycleTimes = []
+coulCount = []
 cycleTimes.append(sheet.cell(row=2, column=2).value.time())
+coulCount.append(sheet.cell(row=2, column=8).value)
 
 print("ROW | VALUE")
 for i in range(2, sheet.max_row):
@@ -17,9 +22,26 @@ for i in range(2, sheet.max_row):
     if int(current) >= 0 and int(next) <= 0 or int(current) <= 0 and int(next) >= 0:
         cycleTimes.append(sheet.cell(row=i, column=2).value)
         cycleTimes.append(sheet.cell(row=(i+1), column=2).value)
+        
+        coulCount.append(sheet.cell(row=i, column=8).value)
+        coulCount.append(sheet.cell(row=(i+1), column=8).value)
         print(i+1, next)
 
 print("\nTIMES")        
 cycleTimes.append(sheet.cell(row=sheet.max_row, column=2).value)        
 for i in cycleTimes:
     print(i)
+
+print("\nTIMES BETWEEN CYCLES")
+for i in cycleTimes:
+    print
+
+print("\nCOLOUMB COUNT")
+coulCount.append(sheet.cell(row=sheet.max_row, column=8).value)
+for i in coulCount:
+    print(i)
+
+
+#wb.create_sheet('test1')
+#wb.save('ngt_log.xlsx')
+
