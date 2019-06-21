99700e70e03b760c4fe173035819c62364d63ffb
Yasir Choudhury <yasirgchoudhury@utexas.edu>
2019-06-20 21:54:27 -0500

    finished 2) and 3)

--- new1.py
+++ new1.py
@@ -6,14 +6,14 @@ import openpyxl
 import datetime
 from datetime import date, datetime, timedelta
 
-os.chdir('C:\\Users\yasirc\Desktop\parseData') # enter correct filepath for project here
+os.chdir('C:\\Users\Yasir\Desktop\parseData') # enter correct filepath for project here
 wb = openpyxl.load_workbook('ngt_log.xlsx') # filename being analyzed
 sheet = wb['sheet1'] # name of the sheet that is being analyzed
 cycleTimes = []
 coulCount = []
+capChange = []
 dateTimes = []
 timeDeltas = []
-res = []
 
 cycleTimes.append(sheet.cell(row=2, column=2).value.time()) # adds 00:00:00 as start time
 coulCount.append(sheet.cell(row=2, column=8).value)
@@ -29,18 +29,34 @@ for i in range(2, sheet.max_row):
         coulCount.append(sheet.cell(row=(i+1), column=8).value)
         print(i+1, next)
 
-print("\nTIMES")        
+print("\nCYCLE CHANGE TIMES")        
 cycleTimes.append(sheet.cell(row=sheet.max_row, column=2).value)        
 for i in cycleTimes:
     print(i)
     dateTimes.append(datetime.combine(date.today(), i))
 
-print("\nTIMES BETWEEN CYCLES")
+print("\nELAPSED TIMES PER CYCLE")
+starts = dateTimes[::2]
+ends = dateTimes[1::2]
+timeDeltas = [end - start for start, end in zip(starts, ends)]
+for i in timeDeltas:
+    print(i)
 
 print("\nCOLOUMB COUNT")
 coulCount.append(sheet.cell(row=sheet.max_row, column=8).value)
 for i in coulCount:
     print(i)
+
+print("\nCAPACITY CHANGE PER CYCLE")
+starts = coulCount[::2]
+ends = coulCount[1::2]
+capChange = [end - start for start, end in zip(starts, ends)]
+for i in capChange:
+    print(i)
+
+
+
+
 
 # input() #keeps cmd window open after script execution
 
